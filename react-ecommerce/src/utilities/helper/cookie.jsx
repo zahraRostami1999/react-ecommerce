45592e49a3b65c1d8bb8832f68323b11f2d00aaf
Zahra Rostami <zahrarostami137878@gmail.com>
2024-11-23 14:08:48 +0330

    edit cookie.jsx: add 3 functions, setCookie, getCookie, removeCookie

---
+++ react-ecommerce/src/utilities/helper/cookie.jsx
@@ -0,0 +1,14 @@
+import Cookies from 'js-cookies';
+import { encryptJWT, decryptJWT } from './jwt';
+
+export const setCookie = async (key, data) => {
+    Cookies.set(key, await encryptJWT(data))
+}
+
+export const getCookie = async (key) => {
+    await decryptJWT(Cookies.get(key))
+}
+
+export const removeCookie = (key) => {
+    Cookies.remove(key);
+}
