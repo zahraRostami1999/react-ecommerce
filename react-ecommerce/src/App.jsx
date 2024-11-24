ca0f0bbb16fe3a1e88f188450cb753074aec5af1
Zahra Rostami <zahrarostami137878@gmail.com>
2024-11-24 09:04:55 +0330

    edit app.jsx: remove ex code

--- react-ecommerce/src/App.jsx
+++ react-ecommerce/src/App.jsx
@@ -1,18 +1,5 @@
-import { setCookie } from './utilities/helper/cookie';
-import { useEffect } from 'react';
-
 function App() {
-  useEffect(() => {
-    const createCookie = async () => {
-      await setCookie("cookie", {
-        name: "zahra", 
-        access_token: "kjdfvjdfvkjdvngkjx", 
-        refresh_token: "mjdavjdfvkjdvnrkjv"
-      })
-    }
 
-    createCookie();
-  }, [])
   return (
     <>This is App page.
 
